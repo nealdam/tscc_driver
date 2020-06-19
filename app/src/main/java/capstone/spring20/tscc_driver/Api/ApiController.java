f21d763e4b612f6794a6d4c153ae8769d481c390
nealdam <neal.dam.1008@gmail.com>
2020-06-19 14:55:42 +0700

    UPDATE IP

--- app/src/main/java/capstone/spring20/tscc_driver/Api/ApiController.java
+++ app/src/main/java/capstone/spring20/tscc_driver/Api/ApiController.java
@@ -2,7 +2,8 @@ package capstone.spring20.tscc_driver.Api;
 
 public class ApiController {
 //    public static String base_url1 = "http://192.168.1.72:5000/";
-    public static String base_url1 = "http://10.0.2.2:5000/";
+  //  public static String base_url1 = "http://10.0.2.2:5000/";
+    public static String base_url1 = "http://192.168.0.113:5000/";
 
 
     public static TSCCDriverClient getTsccDriverClient() {
